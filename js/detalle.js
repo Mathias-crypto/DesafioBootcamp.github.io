8d9b8274524c860c5468c2df07f6b1eab584b41f
Josue <jjosue2015@hotmail.com>
2022-11-07 22:44:26 -0500

    Proyecto

---
+++ js/detalle.js
@@ -0,0 +1,48 @@
+const banderas = document.getElementById('banderas')
+const query = new URLSearchParams(window.location.search)
+const params = query.get('name')
+console.log(params)
+
+document.addEventListener("DOMContentLoaded", e => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('https://restcountries.com/v3.1/all')
+        const data = await res.json()
+        
+        const filtroData = data.filter(item => item.translations.spa.common === params)
+
+        banderillas(filtroData)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const banderillas = data => {
+    let elementos = ''
+    data.forEach(item => {
+        elementos += `
+        <article class="card">
+            <img src="${item.flags.png}" alt="" class="img-fluid info-flag">
+            <div class="card-content">
+                <h3>${item.translations.spa.common}</h3>
+                <p>             
+                    <b>Capital: </b>
+                    ${item.capital}
+                </p>
+                <p>
+                    <b>Población: </b>
+                    ${item.population}
+                </p>
+                <p>
+                    <b>Región: </b>
+                    ${item.region}
+                </p>
+            </div>
+        </article>
+        `
+    });
+    banderas.innerHTML = elementos
+}
